ef63f099c05ca1c5baaf68646f4e1d7470c52fb5
Hinckley28 <144728931+Hinckley28@users.noreply.github.com>
2024-05-05 13:34:30 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ function dijkstra(graph, sourceNode) {
       return null;
   }
   unvisited = [];
-  dist = [];
+  dist = {};
   for (const vertices in graph) {
       unvisited.push(vertices);
       //console.log(unvisited);
@@ -25,7 +25,45 @@ function dijkstra(graph, sourceNode) {
       
       unvisited.shift();
   }
-//console.log(dist);
-const lastVertex = Object.keys(dist).pop();
-return { distance: dist[lastVertex], element: lastVertex };
+
+return dist;
 }
+
+//const fs = require('fs');
+const assert = require('assert');
+//eval(fs.readFileSync('code.js')+'');
+
+A = {
+  'a' : {'b' : 1, 'c' : 4},
+  'b' : {'d' : 1},
+  'c' : {'d' : 5},
+  'd' : {},
+};
+
+B = {
+  
+};
+
+C = {
+  'a' : {'b' : 1, 'c' : 4},
+  'b' : {'d' : 3, 'c' : 1},
+  'c' : {'d' : 1},
+  'd' : {},
+};
+
+D = {
+  'a' : {'b' : 1, 'c' : 1},
+  'b' : {'d' : 4},
+  'c' : {'d' : 2},
+  'd' : {'e' : 6},
+  'e' : {},
+};
+
+
+assert(JSON.stringify(dijkstra(A,'a')) === JSON.stringify({a: 0, b: 1, c: 4, d: 2}));
+
+assert(JSON.stringify(dijkstra(B,' ')) == JSON.stringify({' ': 0}));
+
+assert(JSON.stringify(dijkstra(C, 'a')) == JSON.stringify({a: 0, b: 1, c: 2, d: 3}));
+
+assert(JSON.stringify(dijkstra(D, 'a')) == JSON.stringify({a: 0, b: 1, c: 1, d: 3, e: 9}));
